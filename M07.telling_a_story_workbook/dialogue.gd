c8918578dfe94c320517fae66132f3677abb1250
Shant <shdorsey@s.sfusd.edu>
2025-10-31 11:00:38 -0700

    M7L5

---
+++ M07.telling_a_story_workbook/dialogue.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+@onready var next_button: Button = $NextButton
+
+var dialogue_items: Array[String] = [
+	"Hello there",
+	"My name is Jeff",
+	"My favorite food is tacos",
+]
+
+var current_item_index := 0
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+	
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+	
