5ac82fcbd3f8b4a14cecff5c62ac0ccb01f43516
Shant <shdorsey@s.sfusd.edu>
2025-11-07 10:55:09 -0800

    M7L10

--- M07.telling_a_story_workbook/dialogue.gd
+++ M07.telling_a_story_workbook/dialogue.gd
@@ -1,28 +1,62 @@
 extends Control
 
-@onready var rich_text_label: RichTextLabel = $RichTextLabel
-@onready var next_button: Button = $NextButton
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-var dialogue_items: Array[String] = [
-	"Oh, Hello",
-	"My name is Jeff",
-	"My favorite food is tacos",
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
+}
+
+var dialogue_items: Array[Dictionary] = [
+	{
+		"expression": expressions["regular"],
+		"text": "Hello, my name is Jeff",
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "I'm a student at Balboa High School",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "My favorite food is Tacos!",
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Do you want to be friends?",
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Pleaseeeeeeeee",
+	},
 ]
-
 var current_item_index := 0
+
+
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
 
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
-	rich_text_label.text = current_item
+	rich_text_label.text = current_item["text"]
+	expression.texture = current_item["expression"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration := 1.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+
 
 func advance() -> void:
 	current_item_index += 1
@@ -30,8 +64,4 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
-
-func _ready() -> void:
-	show_text()
-	next_button.pressed.connect(advance)
 	
